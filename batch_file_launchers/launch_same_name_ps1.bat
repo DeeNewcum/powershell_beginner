0ce0d87f26829d65fe7a9a505f75dca21b6909d1
Dee Newcum <newcum@uic.edu>
2023-02-16 12:43:25 -0600

    move "pause" outside of .ps1 so syntax errors are shown

--- batch_file_launchers/launch_same_name_ps1.bat
+++ batch_file_launchers/launch_same_name_ps1.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 :: launch a Powershell script .ps1 from an identically named batch file
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process -NoNewWindow PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0%~n0.ps1""'}" || pause
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process -NoNewWindow -Wait PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0%~n0.ps1""'}"
+pause
--- batch_file_launchers/launch_same_name_ps1.ps1
+++ batch_file_launchers/launch_same_name_ps1.ps1
@@ -1,15 +1,1 @@
-    function ExitWithAnyKey {
-        # If running in the console, wait for input before closing, to make sure the user can see any warnings.
-        if ($Host.Name -eq "ConsoleHost")
-        {
-            Write-Host ""
-            Write-Host "Press any key to continue..."
-            $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
-        }
-        Exit
-    }
-
-
 Write-Output "$($MyInvocation.MyCommand) was successfully run."
-
-ExitWithAnyKey
